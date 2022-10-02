6816fae95508727a58d50c70a7382238e49e9eed
abdullah al harun <abdullahahrn@gmail.com>
2022-10-02 19:03:59 -0400

    hw

--- src/pJava/JavaPractice.java
+++ src/pJava/JavaPractice.java
@@ -1,9 +1,12 @@
 package pJava;
 
+import java.util.Scanner;
+
 public class JavaPractice {
 
-	public static void main(String[] args) {
-		System.out.println("this is for just java practice");
+		public static void main(String[] args) {
+			System.out.println("<-- Please Enter your Hemoglobin A1C  value below -->");
+			Scanner scanner = new Scanner(System.in);
 
 	}
 
